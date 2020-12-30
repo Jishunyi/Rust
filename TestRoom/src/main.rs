6c16f79c1e77d38e786bba979eb72edd33a7c3f2
Jishunyi <jishunyi131@163.com>
2020-12-30 13:55:47 +0800

    Hello Rust!

--- TestRoom/src/main.rs
+++ TestRoom/src/main.rs
@@ -2,10 +2,11 @@
  * @Author       : Shunyi
  * @Date         : 2020-12-30 13:48:08
  * @LastEditors  : Shunyi
- * @LastEditTime : 2020-12-30 13:50:09
+ * @LastEditTime : 2020-12-30 13:54:46
  * @Blog         : https://shunyi.fun/
  * @FilePath     : \Rust\TestRoom\src\main.rs
  */
 fn main() {
-    println!("Hello Rust !");
+    let a = "Hello Rust!";
+    println!("TestRoom is {}", a);
 }
