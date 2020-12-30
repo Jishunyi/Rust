8faaec9c8f23e9f537eb46de277ec3158d491de0
Jishunyi <jishunyi131@163.com>
2020-12-30 13:50:52 +0800

    Hello Rust ！

--- TestRoom/src/main.rs
+++ TestRoom/src/main.rs
@@ -2,10 +2,10 @@
  * @Author       : Shunyi
  * @Date         : 2020-12-30 13:48:08
  * @LastEditors  : Shunyi
- * @LastEditTime : 2020-12-30 13:48:56
+ * @LastEditTime : 2020-12-30 13:50:09
  * @Blog         : https://shunyi.fun/
  * @FilePath     : \Rust\TestRoom\src\main.rs
  */
 fn main() {
-    println!("Hello Rust!");
+    println!("Hello Rust !");
 }
