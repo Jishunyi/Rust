d57397ec0fd3cc03c69a4b44b7d04cfe9ef53676
Jishunyi <jishunyi131@163.com>
2020-12-30 13:35:58 +0800

    Hello Rust!

---
+++ TestRoom/greeting/target/rls/debug/deps/greeting-52086bf610d726d4.d
@@ -0,0 +1,5 @@
+j:\VSCode\Rust\TestRoom\greeting\target\rls\debug\deps\greeting-52086bf610d726d4.rmeta: src\main.rs
+
+j:\VSCode\Rust\TestRoom\greeting\target\rls\debug\deps\greeting-52086bf610d726d4.d: src\main.rs
+
+src\main.rs:
---
+++ TestRoom/greeting/target/rls/debug/deps/greeting-93ef9c10e43d0d8b.d
@@ -0,0 +1,5 @@
+j:\VSCode\Rust\TestRoom\greeting\target\rls\debug\deps\greeting-93ef9c10e43d0d8b.rmeta: src\main.rs
+
+j:\VSCode\Rust\TestRoom\greeting\target\rls\debug\deps\greeting-93ef9c10e43d0d8b.d: src\main.rs
+
+src\main.rs:
