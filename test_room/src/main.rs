f25f8b560163a008ded9312fd1de623ed92ba913
Shunyi <RT006792@rotai.com>
2021-09-16 14:26:01 +0800

    Guess Number!

--- test_room/src/main.rs
+++ test_room/src/main.rs
@@ -2,10 +2,32 @@
  * @Author       : Shunyi
  * @Date         : 2021-09-16 09:35:17
  * @LastEditors  : Shunyi
- * @LastEditTime : 2021-09-16 09:47:46
+ * @LastEditTime : 2021-09-16 14:25:37
  * @Blog         : https://shunyi.fun/
  * @FilePath     : \Rust\test_room\src\main.rs
  */
+use rand::Rng;
+use std::cmp::Ordering;
+use std::io;
 fn main() {
-    println!("Hello, Rust!");
+    println!("Guess the number!");
+    let secret_number = rand::thread_rng().gen_range(1..101);
+    println!("The secret_number is:{}", secret_number);
+    loop {
+        println!("Please input your guess:");
+        let mut guess = String::new();
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line.");
+        let guess: u32 = guess.trim().parse().expect("Please type a number.");
+        println!("Your guess number:{}", guess);
+        match guess.cmp(&secret_number) {
+            Ordering::Equal => {
+                println!("You WIn!");
+                break;
+            }
+            Ordering::Greater => println!("Too Big"),
+            Ordering::Less => println!("Too Small"),
+        }
+    }
 }
