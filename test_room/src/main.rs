6bf5f9808d434442c0cc2136e68ed29d20eb8a54
Shunyi <RT006792@rotai.com>
2021-09-16 09:48:38 +0800

    Rust Start!

--- test_room/src/main.rs
+++ test_room/src/main.rs
@@ -2,10 +2,10 @@
  * @Author       : Shunyi
  * @Date         : 2021-09-16 09:35:17
  * @LastEditors  : Shunyi
- * @LastEditTime : 2021-09-16 09:35:32
+ * @LastEditTime : 2021-09-16 09:47:46
  * @Blog         : https://shunyi.fun/
  * @FilePath     : \Rust\test_room\src\main.rs
  */
 fn main() {
-    println!("Hello, world!");
+    println!("Hello, Rust!");
 }
